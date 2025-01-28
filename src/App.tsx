7b62bb1d7eca15f24f1450ca785beb671a02137c
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2025-01-29 05:31:59 +0900

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Header from "./Components/Header";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL || "/"}>
       <Header />
       <Switch>
         <Route path={["/tv", "/tv/:tvId"]}>
